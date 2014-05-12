397e7bff3a02d21292e226d1b8f576c1643ff999
akil15 <akil15@hotmail.fr>
2014-05-12 17:54:20 +0200

    Update EnterGridPage.java

--- gameoflife-acceptance-tests/src/test/java/com/wakaleo/gameoflife/webtests/pages/EnterGridPage.java
+++ gameoflife-acceptance-tests/src/test/java/com/wakaleo/gameoflife/webtests/pages/EnterGridPage.java
@@ -5,21 +5,22 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.junit.Test;
 
 @DefaultUrl("http://localhost:9090/gameoflife/new")
 public class EnterGridPage extends GameOfLifePage {
 
     @FindBy(id = "submit")
     WebElement goButton;
-
+    @Test
     public EnterGridPage(WebDriver driver) {
         super(driver);
     }
-
+    @Test
     public void clickOnGoButton() {
         clickOn(goButton);
     }
-
+    @Test
     public void clickOnCellAt(int row, int column) {
         String cellName = "cell_" + row + "_" + column;
         getDriver().findElement(By.name(cellName)).click();
