ccce03009616eac3c14c73fb717388d5718d5bc1
akil15 <akil15@hotmail.fr>
2014-05-12 17:55:50 +0200

    Update HomePage.java

--- gameoflife-acceptance-tests/src/test/java/com/wakaleo/gameoflife/webtests/pages/HomePage.java
+++ gameoflife-acceptance-tests/src/test/java/com/wakaleo/gameoflife/webtests/pages/HomePage.java
@@ -4,6 +4,7 @@ import net.thucydides.core.annotations.DefaultUrl;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.junit.Test;
 
 @DefaultUrl("http://localhost:9090/gameoflife/home")
 public class HomePage extends GameOfLifePage {
@@ -14,7 +15,7 @@ public class HomePage extends GameOfLifePage {
     public HomePage(WebDriver driver) {
         super(driver);
     }
-
+    @Test
     public void clickOnNewGameLink() {
         newGameLink.click();
     }
